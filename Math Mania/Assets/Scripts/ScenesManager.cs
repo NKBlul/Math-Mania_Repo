1c1c2967aec7391dc4722c4e788d64ec8312c131
NKBlul <hansenharly123@gmail.com>
2025-11-04 19:26:51 -0500

    added basic main menu to go to minigame

---
+++ Math Mania/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
